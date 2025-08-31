a460fdaeffb00c26879253970ee10e3ead334ed3
Satyasy <satyavpandega@gmail.com>
2025-08-31 18:50:39 +0700

    Add migration table

---
+++ database/migrations/2025_08_31_095507_create_budayas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('budayas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('jenis_budaya');
+            $table->string('nama_budaya');
+            $table->text('deskripsi_budaya');
+            $table->string('asal_daerah');
+            $table->enum('status', ['aktif', 'hampir punah', 'punah'])->default('aktif');
+            // Foreign key constraint
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budayas');
+    }
+};
---
+++ database/migrations/2025_08_31_103747_create_pustakas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Collection;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pustakas', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('author');
+            $table->string('tema');
+            $table->text('sinopsis');
+            $table->date('tahun_terbit');
+            $table->foreignId('budaya_id')->nullable()->constrained('budayas', 'id')->onDelete('set null');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pustakas');
+    }
+};
