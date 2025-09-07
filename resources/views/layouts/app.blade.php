2da3a6c5766eb2286bc18ebebdb7a315709c43ae
ReyCannavaro <reyjunoalcannavaro@gmail.com>
2025-09-07 19:53:40 +0700

    Explore Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nusaya - Melestarikan Budaya</title>
-    @vite(['resources/css/layout.css', 'resources/css/welcome.css', 'resources/css/app.css'])
+    @vite(['resources/css/layout.css', 'resources/css/welcome.css', 'resources/css/explore.css', 'resources/css/budaya.css', 'resources/css/ragam.css', 'resources/css/detail.css', 'resources/css/artikel.css',  'resources/css/app.css'])
 </head>
 <body>
     @include('layouts.navbar')
